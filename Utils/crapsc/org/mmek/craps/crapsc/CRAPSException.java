51538a7f533b54e173bdf15a78ad4d1126db743d
Maxime Arthaud <maxime@arthaud.me>
2015-02-11 21:00:05 +0100

    Remove Utils/crapsc

--- Utils/crapsc/org/mmek/craps/crapsc/CRAPSException.java
+++
@@ -1,9 +0,0 @@
-package org.mmek.craps.crapsc;
-
-public class CRAPSException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public CRAPSException(String a) {
-        super(a);
-    }
-}
