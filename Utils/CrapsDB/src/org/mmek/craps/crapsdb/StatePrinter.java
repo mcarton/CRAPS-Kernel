40d79dd0cebf681d464648035d7a42550c37e0a5
mcarton <cartonmartin+git@gmail.com>
2015-02-12 22:48:13 +0100

    Cleanup StatePrinter

--- Utils/CrapsDB/src/org/mmek/craps/crapsdb/StatePrinter.java
+++ Utils/CrapsDB/src/org/mmek/craps/crapsdb/StatePrinter.java
@@ -15,53 +15,59 @@ public class StatePrinter {
     }
 
     public void printRegisters() throws CommException {
-        System.out.println(Colors.BLUE + "[----------------------------------registers-----------------------------------]" + Colors.ALL_OFF);
+        System.out.println(title("registers"));
 
         // first line : %r1..%r5
         for(int i = 1; i <= 5; i++) {
-            System.out.print(" " + Colors.GREEN + "%r" + i + ":  " + Colors.ALL_OFF + formatHexString(api.readRegister(i)));
+            System.out.print(formatRegister("%r" + i + ":  ", i));
         }
         System.out.println();
 
         // second line : %r6..%r9 %psr
         for(int i = 6; i <= 9; i++) {
-            System.out.print(" " + Colors.GREEN + "%r" + i + ":  " + Colors.ALL_OFF + formatHexString(api.readRegister(i)));
+            System.out.print(formatRegister("%r" + i + ":  ", i));
         }
 
         int psr = (int) api.readRegister(25);
         System.out.print(" " + Colors.GREEN + "%psr: " + Colors.ALL_OFF);
 
         // flags
-        if((psr & 128) > 0) System.out.print(Colors.GREEN);
-        else System.out.print(Colors.RED);
-        System.out.print("N " + Colors.ALL_OFF);
-
-        if((psr & 64) > 0) System.out.print(Colors.GREEN);
-        else System.out.print(Colors.RED);
-        System.out.print("Z " + Colors.ALL_OFF);
-
-        if((psr & 32) > 0) System.out.print(Colors.GREEN);
-        else System.out.print(Colors.RED);
-        System.out.print("V " + Colors.ALL_OFF);
-
-        if((psr & 16) > 0) System.out.print(Colors.GREEN);
-        else System.out.print(Colors.RED);
-        System.out.print("C " + Colors.ALL_OFF);
+        System.out.print(formatFlag(psr, 'N', 128));
+        System.out.print(formatFlag(psr, 'Z',  64));
+        System.out.print(formatFlag(psr, 'V',  32));
+        System.out.print(formatFlag(psr, 'C',  16));
 
         // int
         System.out.println(psr & 0xf);
 
         // last line : %brk %fp %ret %sp %pc
-        System.out.print(" " + Colors.GREEN + "%brk: " + Colors.ALL_OFF + formatHexString(api.readRegister(26)));
-        System.out.print(" " + Colors.GREEN + "%fp:  " + Colors.ALL_OFF + formatHexString(api.readRegister(27)));
-        System.out.print(" " + Colors.GREEN + "%ret: " + Colors.ALL_OFF + formatHexString(api.readRegister(28)));
-        System.out.print(" " + Colors.GREEN + "%sp:  " + Colors.ALL_OFF + formatHexString(api.readRegister(29)));
-        System.out.print(" " + Colors.GREEN + "%pc:  " + Colors.ALL_OFF + formatHexString(api.readRegister(30)));
+        System.out.print(formatRegister("%brk: ", 26));
+        System.out.print(formatRegister("%fp:  ", 27));
+        System.out.print(formatRegister("%ret: ", 28));
+        System.out.print(formatRegister("%sp:  ", 29));
+        System.out.print(formatRegister("%pc:  ", 30));
         System.out.println();
     }
 
+    private String formatRegister(String name, int number) throws CommException {
+        return
+            ' '
+          + Colors.GREEN
+          + name
+          + Colors.ALL_OFF
+          + formatHexString(api.readRegister(number))
+        ;
+    }
+
+    private String formatFlag(int psr, char name, int number) {
+        return
+            (psr & number) > 0 ? Colors.GREEN : Colors.RED
+          + name + ' '
+          + Colors.ALL_OFF;
+    }
+
     public void printAssembly() throws CommException {
-        System.out.println(Colors.BLUE + "[-------------------------------------code-------------------------------------]" + Colors.ALL_OFF);
+        System.out.println(title("code"));
 
         Disassembler disas = new Disassembler(objModule);
         long pc = api.readRegister(30);
@@ -69,52 +75,79 @@ public class StatePrinter {
         for(long addr = Math.max(0, pc - 3); addr <= pc + 5; addr++) {
             System.out.print(" 0x" + formatHexString(addr) + " | ");
 
-            if (objModule != null && objModule.getSym(addr) != null
-                    && !objModule.getSym(addr).isEmpty()) {
+            if (
+                objModule != null
+            && objModule.getSym(addr) != null
+            && !objModule.getSym(addr).isEmpty()
+            ) {
                 String label = objModule.getSym(addr) + ":";
-                if(label.length() > 15) label = label.substring(0, 13) + ":";
+                if(label.length() > 15) {
+                    label = label.substring(0, 13) + ":";
+                }
                 System.out.print(padRight(label, 15, ' ') + "    ");
             }
             else {
-                System.out.print("                   ");
+                System.out.print(padRight("",    19, ' ')         );
             }
 
-            if(addr == pc) System.out.print(Colors.GREEN);
+            if(addr == pc) {
+                System.out.print(Colors.GREEN);
+            }
             System.out.print(disas.disassemble(addr, api.readMemory(addr)));
             System.out.println(Colors.ALL_OFF);
         }
     }
 
     public void printStack() throws CommException {
-        System.out.println(Colors.BLUE + "[------------------------------------stack-------------------------------------]" + Colors.ALL_OFF);
+        System.out.println(title("stack"));
 
         long sp = api.readRegister(29);
 
         for(long addr = sp; addr <= Math.min(sp + 6, 8192); addr++) {
-            System.out.println(Colors.BLUE + " 0x" + formatHexString(addr) + Colors.ALL_OFF
-                             + " | 0x" + formatHexString(api.readMemory(addr)));
+            System.out.println(
+                Colors.BLUE
+              + " 0x" + formatHexString(addr)
+              + Colors.ALL_OFF
+              + " | 0x"
+              + formatHexString(api.readMemory(addr))
+            );
         }
     }
 
     public void printEndLine() {
-        System.out.println(Colors.BLUE + "[------------------------------------------------------------------------------]" + Colors.ALL_OFF);
+        System.out.println(title(""));
     }
 
-    public String formatHexString(long val) {
+    private String formatHexString(long val) {
         return padLeft(Long.toHexString(val), 8, '0');
     }
 
-    public String padLeft(String s, int length, char c) {
+    private String padLeft(String s, int length, char c) {
         StringBuilder sb = new StringBuilder();
         sb.append(s);
         while(sb.length() < length) sb.insert(0, c);
         return sb.toString();
     }
 
-    public String padRight(String s, int length, char c) {
+    private String padRight(String s, int length, char c) {
         StringBuilder sb = new StringBuilder();
         sb.append(s);
         while(sb.length() < length) sb.append(c);
         return sb.toString();
+    }
+
+    private String title(String header) {
+        int left = 40 - 1 + header.length()/2;
+
+        return
+            Colors.BLUE
+          + '['
+          + padRight(
+                padLeft(header, left, '-'),
+                80-2, '-'
+            )
+          + ']'
+          + Colors.ALL_OFF
+        ;
     }
 }
